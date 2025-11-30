0de31bc11e2468fdc309cb55ef0db713c6595060
Tut-Turuu <gffhcvhh@gmail.com>
2025-11-30 19:38:31 +0200

    lab4

--- main.c
+++ main.c
@@ -1,81 +1,111 @@
 #include <stdint.h>
-#include <stm32f10x.h>
 
 
-#define RCC_APB2ENR   (*((volatile uint32_t*)0x40021018))
-#define GPIOC_CRH     (*((volatile uint32_t*)0x40011004))
-#define GPIOC_ODR     (*((volatile uint32_t*)0x4001100C))
-#define GPIOC_IDR     (*((volatile uint32_t*)0x40011008))
 
-const uint32_t delays[] = {
-    256000000,  // 1/64 Гц
-    128000000,  // 1/32 Гц
-    64000000,   // 1/16 Гц
-    32000000,   // 1/8 Гц
-    16000000,   // 1/4 Гц
-    8000000,    // 1/2 Гц
-    4000000,    // 1 Гц
-    2000000,    // 2 Гц
-    1000000,    // 4 Гц
-    500000,     // 8 Гц
-    250000,     // 16 Гц
-    125000,     // 32 Гц
-    62500       // 64 Гц
-};
+#define RCC_APB2ENR   (*(volatile uint32_t*)0x40021018)
+#define GPIOA_CRL     (*(volatile uint32_t*)0x40010800)
+#define GPIOA_ODR     (*(volatile uint32_t*)0x4001080C)
+#define SPI1_CR1      (*(volatile uint32_t*)0x40013000)
+#define SPI1_SR       (*(volatile uint32_t*)0x40013008)
+#define SPI1_DR       (*(volatile uint32_t*)0x4001300C)
 
-const uint32_t delays_count = sizeof(delays);
-volatile int8_t current_step = 6;
+#define CS_PIN   4
+#define DC_PIN   1
+#define RES_PIN  0
 
-static uint8_t last_button = 1;
+#define RCC_APB2ENR_SPI1EN    (1 << 12)
+#define RCC_APB2ENR_IOPAEN    (1 << 2)
 
-void simple_delay(uint32_t cycles) {
-    for(volatile uint32_t i = 0; i < cycles; i++);
+#define SPI_CR1_CPHA          (1 << 0)
+#define SPI_CR1_CPOL          (1 << 1)
+#define SPI_CR1_MSTR          (1 << 2)
+#define SPI_CR1_BR_2          (1 << 5)
+#define SPI_CR1_SSI           (1 << 8)
+#define SPI_CR1_SSM           (1 << 9)
+#define SPI_CR1_SPE           (1 << 6)
+
+#define SPI_SR_TXE            (1 << 1)
+#define SPI_SR_RXNE           (1 << 0)
+
+
+void delay(uint32_t count) {
+    for(volatile uint32_t i = 0; i < count; i++);
 }
 
-void debounce_delay(void) {
-    for(volatile uint32_t i = 0; i < 50000; i++);
+void SPI1_Init(void) {
+    RCC_APB2ENR |= RCC_APB2ENR_SPI1EN | RCC_APB2ENR_IOPAEN;
+    
+    GPIOA_CRL |= (1 << 0) | (1 << 4) | (1 << 16);
+    GPIOA_CRL |= (0xB << 20) | (0xB << 28);
+    
+    SPI1_CR1 |= SPI_CR1_CPHA | SPI_CR1_CPOL | SPI_CR1_MSTR | 
+                SPI_CR1_BR_2 | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE;
+    
+    GPIOA_ODR |= (1 << CS_PIN) | (1 << RES_PIN);
 }
 
-void check_buttons(void) {
-    
-    
-    uint8_t current_button = (GPIOC_IDR & (1 << 14)) ? 1 : 0;
+void SPI1_Write(uint8_t data) {
+    while(!(SPI1_SR & SPI_SR_TXE));
+    SPI1_DR = data;
+}
 
-    if (current_button == 0 && last_button == 1) {
-        debounce_delay();
-        if ((GPIOC_IDR & (1 << 14)) == 0) {
+uint8_t SPI1_Read(void) {
+    SPI1_DR = 0xFF;
+    while(!(SPI1_SR & SPI_SR_RXNE));
+    return SPI1_DR;
+}
+
+void display_cmd(uint8_t cmd) {
+    GPIOA_ODR &= ~(1 << CS_PIN);
+    GPIOA_ODR &= ~(1 << DC_PIN);
+    SPI1_Write(cmd);
+    GPIOA_ODR |= (1 << CS_PIN);
+}
+
+void display_data(uint8_t data) {
+    GPIOA_ODR &= ~(1 << CS_PIN);
+    GPIOA_ODR |= (1 << DC_PIN);
+    SPI1_Write(data);
+    GPIOA_ODR |= (1 << CS_PIN);
+}
+
+void display_init(void) {
+    GPIOA_ODR &= ~(1 << RES_PIN);
+    delay(10000);
+    GPIOA_ODR |= (1 << RES_PIN);
+    delay(10000);
+    
+    display_cmd(0xAE);
+    display_cmd(0x20); 
+    display_cmd(0x00);
+    display_cmd(0x21); 
+    display_cmd(0x00); 
+    display_cmd(0x7F);
+    display_cmd(0x22); 
+    display_cmd(0x00); 
+    display_cmd(0x07);
+    display_cmd(0x8D); 
+    display_cmd(0x14);
+    display_cmd(0xAF);
+}
+
+void display_stripes(void) {
+    for(uint8_t page = 0; page < 8; page++) {
+        display_cmd(0xB0 + page);
+        display_cmd(0x00);
+        display_cmd(0x10);
         
-            current_step = (current_step + 1) % delays_count;
-            
-            while ((GPIOC_IDR & (1 << 14)) == 0); 
-            	debounce_delay();
+        for(uint8_t col = 0; col < 128; col++) {
+            display_data(0b10100010);
         }
     }
-    
-    last_button = current_button;
-
 }
 
 int main(void) {
-    RCC_APB2ENR |= (1 << 4);
-    
-    GPIOC_CRH &= ~(0xFF << 20);
-    GPIOC_CRH |= (1 << 20);
-    
-    GPIOC_CRH &= ~(0xFF << 24);
-    GPIOC_CRH |= (0x8 << 24);
-    GPIOC_ODR |= (1 << 14);
-    
-    GPIOC_CRH &= ~(0xFF << 28);
-    GPIOC_CRH |= (0x8 << 28);
-    GPIOC_ODR |= (1 << 15);
+    SPI1_Init();
+    display_init();
+    display_stripes();
     
     while(1) {
-        GPIOC_ODR ^= (1 << 13);
-        
-        simple_delay(delays[current_step]);
-        
-        check_buttons();
     }
 }
-
